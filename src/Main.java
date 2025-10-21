d6c47e5fa37ea2d8bebc9ea2c199ba967389b2d8
u5erdip <faizzadivana@webmail.umm.ac.id>
2025-10-21 09:55:13 +0700

    program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Git Version Control!");
+        System.out.println("ini program sederhana!");
     }
 }
+//running
+//live demo push ke branch baru
+//debugging pure ingin mencari tau ada error di program kita, fokusnya ingin mencari tau minimalisir error dari programnya, pilih line mana yg ingin di debugging
